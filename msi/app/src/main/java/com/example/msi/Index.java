fbc155d5d55e38604f5b01c0b5cc34e656c9ebd1
unknown <704382210@qq.com>
2019-05-14 16:39:29 +0800

    实现记住密码功能

--- msi/app/src/main/java/com/example/msi/Index.java
+++ msi/app/src/main/java/com/example/msi/Index.java
@@ -1,14 +1,12 @@
 package com.example.msi;
-
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 public class Index extends AppCompatActivity implements View.OnClickListener{
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +17,26 @@ public class Index extends AppCompatActivity implements View.OnClickListener{
         //获取数据的按钮
         Button get_data = (Button)findViewById(R.id.get_data);
         get_data.setOnClickListener(this);
+        //跳转登录界面
+        Button goto_login = (Button)findViewById(R.id.goto_login);
+        goto_login.setOnClickListener(this);
     }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()){
+            case R.id.goto_login:
+                Intent i = new Intent(Index.this,Login.class);
+                startActivity(i);
+                break;
             case R.id.get_data:
                 SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
                 String name = pref.getString("name","");
                 int age = pref.getInt("age",1);
                 String job = pref.getString("job","");
                 boolean married = pref.getBoolean("married",false);
-                Toast.makeText(Index.this,name + "的年龄是：" + age + ",职业是：" + job + ",是否结婚：" + married,Toast.LENGTH_LONG).show();
+                Toast.makeText(Index.this,name + "的年龄是：" +
+                        age + ",职业是：" + job + "," +
+                        "是否结婚：" + married,Toast.LENGTH_LONG).show();
                 break;
             case R.id.save_data:
                 //使用键值对存储数据
